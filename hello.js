aac4c8e469a68d6e998c2c1d409d41494587ce19
mwissman <mwissman@bnl.gov>
2022-09-11 16:56:02 -0700

    commenting added

--- hello.js
+++ hello.js
@@ -1,8 +1,14 @@
+//color chosen by user
 var colorChosen = null;
+//image position
 var pos = 0;
+//interval for animation
 var interval = null;
+//image for animation
 let image = null;
 
+/*sets color of screen background based on user's
+chosen color (defualt is white)*/
 function changeBackgroundColor()
 {
 
@@ -15,27 +21,35 @@ function changeBackgroundColor()
    }
 }
 
+//captures color chosen by user
 function setColor(colorString)
 {
    colorChosen = colorString;
 }
 
+//displays x-coordinate of image
 function xCheck()
 {
    alert(image.offsetTop);
 }
 
+/*sets pointer to image and set image position
+ to relative*/
 function setImage()
 {
    image = document.getElementById("image");
    image.style.position = "relative";
 }
 
+/*sets movement of image to occur
+every 5 millisecs*/
 function setAnime()
 {
    setInterval(moveAnime, 5);
 }
 
+/*moves image 1px down until image
+top at 350px*/
 function moveAnime()
 {
    if(pos == 350)
