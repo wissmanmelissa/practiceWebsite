f2c8abf730653ca8ae8f4bb6d4489e132953e69c
mwissman <mwissman@bnl.gov>
2022-11-05 12:28:48 -0700

    server listening on local machine port 80

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 //uploads Node.js "http" module
-const http = require("http");
+const http = require('http');
 //sets host as local machine
 const host = 'localhost';
-//sets listening port as 8000
-const port = 8000;
+//sets listening port as 80
+const port = 80;
 
 //creates listening server at the local machine's port 8000
 const requestListener = function (req, res) 
 {
-    
+    if(req.method == 'POST')
+    {
+        console.log(`Request made`);
+    }
 };
+
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
+server.listen(port, host, () => 
+{
     console.log(`Server is running on http://${host}:${port}`);
 });
